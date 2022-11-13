7c77b0ef7fc7eb2accf0137ad37ccf70c1215340
mhamad <mhamad.hmd20@gmail.com>
2022-11-13 20:00:46 +0200

    pc

--- src/Components/swordAnimation/SwordAnimation.tsx
+++ src/Components/swordAnimation/SwordAnimation.tsx
@@ -18,7 +18,10 @@ const SwordAnimation = () => {
     const swordSpan = document.getElementById('swordSvg')!;
     const swordSvgPlace = document.getElementById('swordSvgPlace')!.getBoundingClientRect();
     const shopNow = document.getElementById('shopNow')!.getBoundingClientRect();
-    console.log(shopNow)
+    const learnMore = document.getElementById('learnMore')!.getBoundingClientRect();
+    const cart = document.getElementById('cart')!.getBoundingClientRect();
+
+    console.log("btn:",learnMore, " sword:",swordSvgPlace)
     
 const animate = (target:any) => {
     swordSpan.style.top = `${target.top}px`;
@@ -28,9 +31,22 @@ const animate = (target:any) => {
 }
 
     animate(swordSvgPlace)
+   
+    function getAngle (x1, y1, x2, y2) {
+        var distY = Math.abs(y2-y1); //opposite
+        var distX = Math.abs(x2-x1); //adjacent
+        var dist = Math.sqrt((distY*distY)+(distX*distX)); //hypotenuse, 
+           //don't know if there is a built in JS function to do the square of a number
+        var val = distY/dist;
+        var aSine = Math.asin(val);
+        return aSine*(180/Math.PI); //return angle in degrees
+    }
 
-    
+    const rotateTo = getAngle(swordSvgPlace.x, swordSvgPlace.y, cart.x, cart.y )
 
+
+console.log(getAngle(screenX, screenY, cart.x, cart.y ))
+swordSpan.style.rotate =  `${rotateTo}deg`
 
 }
 
