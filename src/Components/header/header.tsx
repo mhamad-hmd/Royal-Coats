81e1b38152a6369a666e4a1293a3d2d64cfa10dc
mhamad-hmd <107796257+mhamad-hmd@users.noreply.github.com>
2022-11-20 13:03:10 +0200

    lptp

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -6,20 +6,24 @@ const Header = () => {
     const swordPath = document.getElementById('swordPath')
     const searchIcon = document.getElementById('searchIcon')
     const searchBarWrapper = document.getElementById('searchBarWrapper')
-
-    let showHeader = 0;
+    const searchBarContainer = document.getElementById('searchBarContainer')
+    const itemListHeader = document.getElementById('itemListHeader2')
+    const headerSubMenuContainer = document.getElementById('headerSubMenuContainer2')
+    const headerSubMenuWrapper = document.getElementById('headerSubMenuWrapper2')
     
+    let showHeader = 0;
 
-        window!.onscroll = () => {
-           if (window.scrollY == 0){
+
+    window!.onscroll = () => {
+        if (window.scrollY == 0) {
             headerContainer!.style.position = "absolute"
             headerContainer!.style.background = "rgba(180, 177, 177, 0)"
             announcement!.style.display = "flex"
             swordPath!.style.fill = "rgb(180, 177, 177)"
             searchBarWrapper!.style.background = "rgba(180, 177, 177, 0)"
 
-           }
-           else{
+        }
+        else {
             announcement!.style.display = "none"
             headerContainer!.style.position = "fixed"
             headerContainer!.style.background = "rgba(180, 177, 177, 100)"
@@ -27,22 +31,58 @@ const Header = () => {
             searchBarWrapper!.style.background = "rgba(180, 177, 177, 100)"
 
         }
+    }
+
+    const hideNode = (element, parentElement, status) => {
+        if(status === "show"){
+            element!.classList.remove('collapseNode')
+            element!.classList.add('showNode')
+            parentElement!.style.visibility = "visible"
         }
-
-        searchIcon!.onclick = () => {
-
-            if(showHeader % 2 !== 0){
-
-                searchBarWrapper!.classList.add('collapseHeader')
-                searchBarWrapper!.classList.remove('show')
-            }
-            else if(showHeader % 2 === 0){
-                searchBarWrapper!.classList.remove('collapseHeader')
-                searchBarWrapper!.classList.add('show')
-            }
-            showHeader++
+        else if (status === "hide") {
+            element!.classList.add('collapseNode')
+            element!.classList.remove('showNode')
+            parentElement!.style.visibility = "collapse"
         }
-    
+    }
+
+    searchIcon!.onclick = () => {
+        showHeader++
+
+        if (showHeader % 2 !== 0) {
+            hideNode(searchBarWrapper, searchBarContainer, "show" )
+        }
+        else if (showHeader % 2 == 0) {
+            hideNode(searchBarWrapper, searchBarContainer, "hide" )
+        }
+    }
+
+    const handleClickOutsideSearchBar = (e: any) => {
+        if (!searchBarWrapper!.contains(e.target) && !searchIcon!.contains(e.target)) {
+            hideNode(searchBarWrapper, searchBarContainer, "hide" )
+            showHeader = 0
+        }
+    }
+    document.addEventListener('click', (e) => handleClickOutsideSearchBar(e))
+
+
+    // itemListHeader!.onmouseover = () => {
+    //     hideNode(headerSubMenuWrapper, headerSubMenuContainer, 'show')
+    //     console.log(headerSubMenuWrapper!.onmouseleave && itemListHeader!.onmouseleave)
+
+    // }
+
+    // itemListHeader!.onmouseout = () => {
+    //     mousenList
+    // }
+
+    // headerSubMenuWrapper!.onmouseout = (e) => {
+    //     if(headerSubMenuWrapper!.onmouseout && itemListHeader!.onmouseover){
+    //         hideNode(headerSubMenuWrapper, headerSubMenuContainer, 'hide')
+    //     }
+    // }
+
+
 
 }
 
