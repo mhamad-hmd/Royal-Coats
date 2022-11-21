42ee7bb134e6d14085fe103eacd0f6b934c98030
mhamad <mhamad.hmd20@gmail.com>
2022-11-21 21:05:14 +0200

    pc

--- src/Components/header/header.tsx
+++ src/Components/header/header.tsx
@@ -2,45 +2,41 @@ import React, { useState } from 'react'
 
 const Header = () => {
     const headerContainer = document.getElementById('headerContainer');
+    const headerWrapper = document.getElementById('headerWrapper');
     const announcement = document.getElementById('announcement')
-    const swordPath = document.getElementById('swordPath')
-    const searchIcon = document.getElementById('searchIcon')
-    const searchBarWrapper = document.getElementById('searchBarWrapper')
-    const searchBarContainer = document.getElementById('searchBarContainer')
-    const itemListHeader = document.getElementById('itemListHeader2')
-    const headerSubMenuContainer = document.getElementById('headerSubMenuContainer2')
-    const headerSubMenuWrapper = document.getElementById('headerSubMenuWrapper2')
-    
+    const swordPath = document.getElementById('swordPath');
+    const searchIcon = document.getElementById('searchIcon');
+    const searchBarWrapper = document.getElementById('searchBarWrapper');
+    const searchBarContainer = document.getElementById('searchBarContainer');
+
     let showHeader = 0;
     let lastScrollTop = 0
 
     window!.onscroll = () => {
         if (window.scrollY == 0) {
-            headerContainer!.style.background = "rgba(180, 177, 177, 0)"
-            announcement!.style.display = "flex"
+            headerContainer!.classList.remove('annUp');
             swordPath!.style.fill = "rgb(180, 177, 177)"
-            searchBarWrapper!.style.background = "rgba(180, 177, 177, 0)"
+            searchBarWrapper!.style.background = "rgba(246, 243, 240, 0)"
 
         }
         else {
-            announcement!.style.display = "none"
-            headerContainer!.style.background = "rgba(180, 177, 177, 100)"
+            headerContainer!.classList.add('annUp')
             swordPath!.style.fill = "rgb(102, 104, 106)"
-            searchBarWrapper!.style.background = "rgba(180, 177, 177, 100)"
+            searchBarWrapper!.style.background = "rgba(246, 243, 240, 100)"
 
         }
 
         const st = window.pageYOffset
-        if(st > lastScrollTop){
-
-        } else if(st < lastScrollTop){
-            console.log("scrolling Up")
+        if (st > lastScrollTop) {
+            headerContainer!.classList.add('navUp')
+        } else if (st < lastScrollTop) {
+            headerContainer!.classList.remove('navUp')
         }
         lastScrollTop = st <= 0 ? 0 : st
     }
 
     const hideNode = (element, parentElement, status) => {
-        if(status === "show"){
+        if (status === "show") {
             element!.classList.remove('collapseNode')
             element!.classList.add('showNode')
             parentElement!.style.visibility = "visible"
@@ -56,16 +52,16 @@ const Header = () => {
         showHeader++
 
         if (showHeader % 2 !== 0) {
-            hideNode(searchBarWrapper, searchBarContainer, "show" )
+            hideNode(searchBarWrapper, searchBarContainer, "show")
         }
         else if (showHeader % 2 == 0) {
-            hideNode(searchBarWrapper, searchBarContainer, "hide" )
+            hideNode(searchBarWrapper, searchBarContainer, "hide")
         }
     }
 
     const handleClickOutsideSearchBar = (e: any) => {
         if (!searchBarWrapper!.contains(e.target) && !searchIcon!.contains(e.target)) {
-            hideNode(searchBarWrapper, searchBarContainer, "hide" )
+            hideNode(searchBarWrapper, searchBarContainer, "hide")
             showHeader = 0
         }
     }
